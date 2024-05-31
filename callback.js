e02d35c3b8c3ca72ab8c9d27127a3215403fc8a8
Mizbey <f903471@gmail.com>
2024-05-31 07:23:50 -0700

    Update callback.js

--- callback.js
+++ callback.js
@@ -1,1 +1,13 @@
+// api/callback.js
+
+module.exports = (req, res) => {
+    const { code } = req.query;
+
+    const redirectUrl = `https://nichenexus2.blogspot.com/p/callback-handler.html?code=${code}`;
+
+    res.writeHead(302, {
+        Location: redirectUrl
+    });
+    res.end();
+};
 
